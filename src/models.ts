93e7abcef2489ec121710fbbe1f888f68ca4bc94
Eric Wang <wrqatw@gmail.com>
2023-07-19 22:05:18 +0800

    models: rm unnecessary fields
    wip: refactor the proxy

--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,8 @@
 import { Model, ListModelsResponse } from 'openai';
 
+// TODO use a configurable toml file to store this
+
+// TODO rm this
 const filterChatCompatibleModels = (response: ListModelsResponse): Array<Model> => {
 	const chatCompatibleModels = [
 		'gpt-4',
@@ -34,215 +37,56 @@ const openaiResponse: ListModelsResponse = {
 			object: 'model',
 			created: 1685474247,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-ZY0iXVEnYcuTmNTeVNoZLg0n',
-					object: 'model_permission',
-					created: 1688692724,
-					allow_create_engine: false,
-					allow_sampling: true,
-					allow_logprobs: true,
-					allow_search_indices: false,
-					allow_view: true,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-3.5-turbo-16k-0613',
-			parent: null,
 		},
 		{
 			id: 'gpt-3.5-turbo-0301',
 			object: 'model',
 			created: 1677649963,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-TBa0NeEwCp3BQtV3fxDVx2fs',
-					object: 'model_permission',
-					created: 1689207811,
-					allow_create_engine: false,
-					allow_sampling: true,
-					allow_logprobs: true,
-					allow_search_indices: false,
-					allow_view: true,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-3.5-turbo-0301',
-			parent: null,
 		},
 		{
 			id: 'gpt-3.5-turbo-16k',
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'openai-internal',
-			permission: [
-				{
-					id: 'modelperm-incf1vHEBCbZnCddTGBKniux',
-					object: 'model_permission',
-					created: 1688692820,
-					allow_create_engine: false,
-					allow_sampling: true,
-					allow_logprobs: true,
-					allow_search_indices: false,
-					allow_view: true,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-3.5-turbo-16k',
-			parent: null,
 		},
 		{
 			id: 'gpt-4-0314',
 			object: 'model',
 			created: 1687882410,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-p5ay61B8HMtrn6P8RCLOcANc',
-					object: 'model_permission',
-					created: 1689084227,
-					allow_create_engine: false,
-					allow_sampling: false,
-					allow_logprobs: false,
-					allow_search_indices: false,
-					allow_view: false,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-4-0314',
-			parent: null,
 		},
 		{
 			id: 'gpt-3.5-turbo',
 			object: 'model',
 			created: 1677610602,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-gF0u9UfHJRjpblMAfUH9McFM',
-					object: 'model_permission',
-					created: 1689189326,
-					allow_create_engine: false,
-					allow_sampling: true,
-					allow_logprobs: true,
-					allow_search_indices: false,
-					allow_view: true,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-3.5-turbo',
-			parent: null,
 		},
 		{
 			id: 'gpt-3.5-turbo-0613',
 			object: 'model',
 			created: 1686587434,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-dusUadKLXIZuBeoRdmZcSDL8',
-					object: 'model_permission',
-					created: 1689189356,
-					allow_create_engine: false,
-					allow_sampling: true,
-					allow_logprobs: true,
-					allow_search_indices: false,
-					allow_view: true,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-3.5-turbo-0613',
-			parent: null,
 		},
 		{
 			id: 'gpt-4',
 			object: 'model',
 			created: 1687882411,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-glQVPpxp3tP8UNTh6k8U3HSf',
-					object: 'model_permission',
-					created: 1689189635,
-					allow_create_engine: false,
-					allow_sampling: false,
-					allow_logprobs: false,
-					allow_search_indices: false,
-					allow_view: false,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-4',
-			parent: null,
 		},
 		{
 			id: 'gpt-4-0613',
 			object: 'model',
 			created: 1686588896,
 			owned_by: 'openai',
-			permission: [
-				{
-					id: 'modelperm-mDfxFax8HtDjp4F3BsTq9aNX',
-					object: 'model_permission',
-					created: 1689189651,
-					allow_create_engine: false,
-					allow_sampling: false,
-					allow_logprobs: false,
-					allow_search_indices: false,
-					allow_view: false,
-					allow_fine_tuning: false,
-					organization: '*',
-					group: null,
-					is_blocking: false,
-				},
-			],
-			root: 'gpt-4-0613',
-			parent: null,
 		},
 	],
-};
-
-const fakePermission = {
-	id: 'modelperm-incf1vHEBCbZnCddTGBKniux',
-	object: 'model_permission',
-	created: 1688692820,
-	allow_create_engine: false,
-	allow_sampling: true,
-	allow_logprobs: true,
-	allow_search_indices: false,
-	allow_view: true,
-	allow_fine_tuning: false,
-	organization: '*',
-	group: null,
-	is_blocking: false,
 };
 
 // The Azure openai service model id are vary by the deployment name you choose.
 // Deployment list can be retrieved with the REST API, read more in:
 // https://learn.microsoft.com/en-us/rest/api/cognitiveservices/accountmanagement/deployments/list?tabs=HTTP#code-try-0
-const fakeAzureResponse: ModelListResponse = {
+const fakeAzureResponse: ListModelsResponse = {
 	object: 'list',
 	data: [
 		{
@@ -250,25 +94,19 @@ const fakeAzureResponse: ModelListResponse = {
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'azure-openai',
-			permission: [fakePermission],
-			root: 'gpt-3.5-turbo',
-			parent: null,
 		},
 		{
 			id: 'gpt-35-turbo-16k',
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'azure-openai',
-			permission: [fakePermission],
-			root: 'gpt-3.5-turbo-16k',
-			parent: null,
 		},
 	],
 };
 
 // Claude models
 // https://docs.anthropic.com/claude/reference/selecting-a-model
-const fakeClaudeResponse: ModelListResponse = {
+const fakeClaudeResponse: ListModelsResponse = {
 	object: 'list',
 	data: [
 		{
@@ -276,36 +114,24 @@ const fakeClaudeResponse: ModelListResponse = {
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'claude',
-			permission: [fakePermission],
-			root: 'claude-instant-1',
-			parent: null,
 		},
 		{
 			id: 'claude-instant-1.1',
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'claude',
-			permission: [fakePermission],
-			root: 'claude-instant-1.1',
-			parent: null,
 		},
 		{
 			id: 'claude-2',
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'claude',
-			permission: [fakePermission],
-			root: 'claude-2',
-			parent: null,
 		},
 		{
 			id: 'claude-2.0',
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'claude',
-			permission: [fakePermission],
-			root: 'claude-2.0',
-			parent: null,
 		},
 	],
 };
@@ -320,28 +146,19 @@ const fakePalmResponse: ListModelsResponse = {
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'palm',
-			permission: [fakePermission],
-			root: 'text-bison-001',
-			parent: null,
 		},
 		{
 			id: 'chat-bison-001',
 			object: 'model',
 			created: 1683758102,
 			owned_by: 'palm',
-			permission: [fakePermission],
-			root: 'chat-bison-001',
-			parent: null,
 		},
-		{
-			id: 'embedding-gecko-001',
-			object: 'model',
-			created: 1683758102,
-			owned_by: 'palm',
-			permission: [],
-			root: 'embedding-gecko-001',
-			parent: null,
-		},
+		// {
+		// 	id: 'embedding-gecko-001',
+		// 	object: 'model',
+		// 	created: 1683758102,
+		// 	owned_by: 'palm',
+		// },
 	],
 };
 
