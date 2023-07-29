057ec4f0039497ca778b1ee45308a951f4111744
Eric Wang <wrqatw@gmail.com>
2023-07-29 21:49:54 +0800

    using itty-router

--- src/worker.ts
+++ src/worker.ts
@@ -8,46 +8,51 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { Router, IRequest } from 'itty-router';
 import handleProxy from './proxy';
 import models from './models';
 
-export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		switch (request.method) {
-			case 'POST':
-				// Validate the Authorization API key is in the worker environment variables
-				const authHeader = request.headers.get('Authorization');
-				if (!authHeader || authHeader !== `Bearer ${env.API_KEY}`) {
-					return new Response('Unauthorized', { status: 401 });
-				}
-
-				const url = new URL(request.url);
-				switch (url.pathname) {
-					case '/v1/models':
-						return new Response(JSON.stringify(models), { headers: { 'Content-Type': 'application/json' } });
-					case '/v1/chat/completions':
-						return handleProxy.fetch(request, env, ctx);
-					default:
-						return new Response('Not Found', { status: 404 });
-				}
-			case 'OPTIONS':
-				return handleOptions();
-			default:
-				// TODO: add an about/into page maybe.
-				return new Response('Not Allowed', { status: 403 });
-		}
-	},
+// declare what's available in our env
+type Env = {
+	API_KEY: string;
 };
 
-function handleOptions() {
-	return new Response(null, {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': '*',
-			'Access-Control-Allow-Headers': '*',
-			'Access-Control-Allow-Credentials': 'true',
-		},
-	});
-}
+// create a convenient duple
+type CF = [env: Env, context: ExecutionContext];
+
+const router = Router<IRequest, CF>();
+
+router
+	.options(
+		'*',
+		() =>
+			new Response(null, {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': '*',
+					'Access-Control-Allow-Headers': '*',
+					'Access-Control-Allow-Credentials': 'true',
+				},
+			})
+	)
+	.post('/v1/models', (request, env) => {
+		const authHeader = request.headers.get('Authorization');
+		if (!authHeader || authHeader !== `Bearer ${env.API_KEY}`) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+		return new Response(JSON.stringify(models), { headers: { 'Content-Type': 'application/json' } });
+	})
+	.post('/v1/chat/completions', (request, env, ctx) => {
+		const authHeader = request.headers.get('Authorization');
+		if (!authHeader || authHeader !== `Bearer ${env.API_KEY}`) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+		return handleProxy.fetch(request, env, ctx);
+	})
+	.all('*', () => new Response('Not Allowed', { status: 403 }));
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return router.handle(request, env, ctx);
+	},
+};
